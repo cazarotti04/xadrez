520835166ca121ddd1664d5c275a0c5b0c0c2a25
cazarotti04 <13874076601@est.udicentro.local>
2016-09-26 09:22:16 -0300

    CARA DO CTIC fez seu primeiro commit

---
+++ proj/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+namespace proj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um valor inteiro: ");
+
+            int x = int.Parse(Console.ReadLine());
+
+            int y = 2 * x;
+
+            Console.WriteLine("Dobro = " + y);
+
+            Console.ReadLine();
+        }
+    }
+}
